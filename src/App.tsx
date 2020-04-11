826103eb964e69029cbd35be40a9997b5c70a4ca
EugeneZeng <eugene@kobuhealth.com>
2020-04-11 17:07:50 +0800

    Added  loading status and error message displaying.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Selector from './Selector';
 import { getBreeds, getImages } from './api';
 import './App.css';
+import logo from './logo.svg';
 
 interface BreedMap {
   [Prop: string]: string[]
@@ -13,6 +14,8 @@ interface Dog {
 let breedMap: BreedMap = {};
 
 function App() {
+  const [errorMsg, setErrorMsg] = useState<string>();
+  const [isImagesLoading, setIsImagesLoading] = useState<boolean>(false);
   const [breeds, setBreeds] = useState<string[]>([]);
   const [subBreeds, setSubBreeds] = useState<string[]>([]);
   const [images, setImages] = useState<string[]>([]);
@@ -21,7 +24,6 @@ function App() {
 
   if (breeds.length === 0) {
     getBreeds().then(res => {
-      console.log(res);
       const dogList: Dog[] = res.data;
       breedMap = {};
       dogList.forEach(dog => {
@@ -31,20 +33,30 @@ function App() {
         }
       });
       setBreeds(Object.keys(breedMap));
+    }).catch(e => {
+      console.error(e);
+      setErrorMsg(e);
     });
   }
 
-  useEffect(()=>{
-    if(breedMap[selectedBreed] && breedMap[selectedBreed].length){
-      setSubBreeds(breedMap[selectedBreed]);
-    } else {
-      getBreedImages(selectedBreed);
+  useEffect(() => {
+    if(errorMsg !== ''){
+      setTimeout(function(){
+        setErrorMsg('');
+      }, 500)
     }
+  }, [errorMsg])
+
+  useEffect(() => {
+    if (breedMap[selectedBreed]) {
+      setSubBreeds(breedMap[selectedBreed]);
+    }
+    getBreedImages(selectedBreed);
   }, [selectedBreed]);
 
   useEffect(() => {
     getBreedImages(selectedBreed, selectedSubBreed);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedSubBreed])
 
   const onBreedChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -54,10 +66,16 @@ function App() {
     setSelectedSubBreed(e.target.value);
   }
 
-  const getBreedImages = (breed:string, subBreed?:string) => {
+  const getBreedImages = (breed: string, subBreed?: string) => {
+    if (breed === '') return;
+    setImages([]);
+    setIsImagesLoading(true);
     getImages(breed, subBreed).then(res => {
-      console.log(res);
       setImages(res.data);
+      setIsImagesLoading(false);
+    }).catch(e => {
+      console.error(e);
+      setErrorMsg(e);
     })
   }
 
@@ -67,7 +85,7 @@ function App() {
         {
           breeds.length ?
             (<Selector onChange={onBreedChange} list={breeds}></Selector>)
-            : (<select><option>---</option></select>)
+            : (<select disabled><option>--Loading breeds--</option></select>)
         }
         {
           subBreeds.length ? (<Selector onChange={onSubBreedChange} list={subBreeds}></Selector>) : null
@@ -75,9 +93,17 @@ function App() {
       </header>
       <article className="APP-article">
         {
-          images.length ? images.map((image, i) => (<img key={i} alt="" src={image} />)) : null
+          images.length ?
+            images.map((image, i) => (<img key={i} alt={[setSelectedBreed, selectedSubBreed].join('-')} src={image} />))
+            : (<span>{isImagesLoading ? 'Image loading...' : 'No images here!'}</span>)
+        }
+        {
+          isImagesLoading && (<img src={logo} className="App-logo" alt="logo" />)
         }
       </article>
+      {
+        errorMsg && (<div className="App-message warn">{errorMsg}</div>)
+      }
     </div>
   );
 }
