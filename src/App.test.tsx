89c2e78ce6381408f733437b239ab9f611d7e570
EugeneZeng <eugene@kobuhealth.com>
2020-04-12 10:25:29 +0800

    Added some tests.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders no image text.', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/No images here!/i);
   expect(linkElement).toBeInTheDocument();
+});
+test('render select and send request.', async () => {
+  const { getByDisplayValue } = render(<App />);
+  expect(getByDisplayValue('--Loading breeds--')).toBeInTheDocument();
 });
