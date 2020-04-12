117ce92e4a483ed0da106ee6e1e3e92cf1224021
EugeneZeng <eugene@kobuhealth.com>
2020-04-12 11:51:59 +0800

    Switch to get api endpoint from env.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
       app.use(
            proxy.createProxyMiddleware("/api", {
-                 target: "http://localhost:3000/",
+                 target: process.env.API_ENDPOINT,
                  changeOrigin: true,
                  pathRewrite: function (path, req) { return path.replace('/api', ''); }
            })
